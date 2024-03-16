d3087aa3a4c6a7379e560a1ea4d5213887fb9031
Florin Popa <456185+FlorinPopaCodes@users.noreply.github.com>
2024-03-16 20:11:49 +0200

    Scrapy multithreading

--- app/server.rb
+++ app/server.rb
@@ -7,15 +7,17 @@ class YourRedisServer
 
   def start
     # You can use print statements as follows for debugging, they'll be visible when running tests.
-    puts("Logs from your program will appear here!")
+    # puts("Logs from your program will appear here!")
 
+    # TODO: EventLoop
     server = TCPServer.new(@port)
     loop do
-      client = server.accept
-      while line = client.gets
-        client.puts "+PONG\r\n" if line.downcase.start_with?('ping')
+      Thread.new(server.accept) do |client|
+        while line = client.gets
+          client.puts "+PONG\r\n" if line.downcase.start_with?('ping')
+        end
+        client.close
       end
-      client.close
     end
   end
 end
